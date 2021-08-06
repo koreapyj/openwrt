b88397cdf43fbfa7be09b4637a40543709cd5281
Yoonji Park <koreapyj@dcmys.kr>
2021-08-06 14:05:47 +0900

    add: iptime a6004mx

--- target/linux/mediatek/image/mt7629.mk
+++ target/linux/mediatek/image/mt7629.mk
@@ -7,3 +7,12 @@ define Device/mediatek_mt7629-rfb
   DEVICE_PACKAGES := swconfig
 endef
 TARGET_DEVICES += mediatek_mt7629-rfb
+
+define Device/iptime_a6004mx
+  DEVICE_VENDOR := ipTIME
+  DEVICE_MODEL := A6004MX
+  DEVICE_DTS := mt7629-iptime-a6004mx
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_PACKAGES := swconfig
+endef
+TARGET_DEVICES += iptime_a6004mx
