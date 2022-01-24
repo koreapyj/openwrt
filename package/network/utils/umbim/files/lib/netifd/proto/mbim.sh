aa95b951d20e04574ea6fafc77cc18657666c035
Yoonji Park <koreapyj@dcmys.kr>
2022-01-24 16:52:17 +0900

    umbim: add firewall zone and ip46table

--- package/network/utils/umbim/files/lib/netifd/proto/mbim.sh
+++ package/network/utils/umbim/files/lib/netifd/proto/mbim.sh
@@ -134,11 +134,15 @@ _proto_mbim_setup() {
 	proto_init_update "$ifname" 1
 	proto_send_update "$interface"
 
+	local zone="$(fw3 -q network "$interface" 2>/dev/null)"
+
 	json_init
 	json_add_string name "${interface}_4"
 	json_add_string ifname "@$interface"
 	json_add_string proto "dhcp"
+	[ -n "$ip4table" ] && json_add_string ip4table "$ip4table"
 	proto_add_dynamic_defaults
+	[ -n "$zone" ] && json_add_string zone "$zone"
 	json_close_object
 	ubus call network add_dynamic "$(json_dump)"
 
@@ -146,8 +150,10 @@ _proto_mbim_setup() {
 	json_add_string name "${interface}_6"
 	json_add_string ifname "@$interface"
 	json_add_string proto "dhcpv6"
+	[ -n "$ip6table" ] && json_add_string ip6table "$ip6table"
 	json_add_string extendprefix 1
 	proto_add_dynamic_defaults
+	[ -n "$zone" ] && json_add_string zone "$zone"
 	ubus call network add_dynamic "$(json_dump)"
 }
 
